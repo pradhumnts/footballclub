c5d925767738c236fe91539649e6f94a7567c360
pradhumnts <pradhumnts@gmail.com>
2021-07-10 15:02:52 +0530

    second commit

--- footballclub/posts/views.py
+++ footballclub/posts/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    heading = "Post 1"
-    article = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam et magnam fugit fuga."
+    heading = ["Post 1", "Post 2", "Post 3"]
+    article = [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam et magnam fugit fuga.", 
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam et magnam fugit fuga.", 
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam et magnam fugit fuga."
+    ]
+    
     res = {'heading':heading, 'article':article}
 
     return render(request, 'posts/home.html', {'res':res})
